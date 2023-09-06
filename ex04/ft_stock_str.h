d5403f7db4b2e1a5a6d2239ab2ef3c86526ee0f6
Thomas Ruello <truello@c2r3p1.42perpignan.fr>
2023-09-06 19:38:15 +0200

    Final Push

---
+++ ex04/ft_stock_str.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: truello <thomasdelan2@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 18:48:44 by truello           #+#    #+#             */
+/*   Updated: 2023/09/06 19:36:08 by truello          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}	t_stock_str;
+
+#endif
