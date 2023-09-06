3fba49cef300374191d1c4d0669a1adffde56dcb
Thomas Ruello <truello@c2r3p1.42perpignan.fr>
2023-09-06 14:27:29 +0200

    Final Push

---
+++ ex03/ft_point.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: truello <thomasdelan2@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 14:23:47 by truello           #+#    #+#             */
+/*   Updated: 2023/09/06 14:27:18 by truello          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_POINT_H
+# define FT_POINT_H
+
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}	t_point;
+
+void	set_point(t_point *point);
+
+#endif
